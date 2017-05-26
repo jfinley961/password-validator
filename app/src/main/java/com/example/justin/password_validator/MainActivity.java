e56836f11e44552feba054c5fdb0ea253093f2c2
jfinley961 <justin.finley1@gmail.com>
2017-05-26 19:50:08 -0300

    Added a simple UI

--- app/src/main/java/com/example/justin/password_validator/MainActivity.java
+++ app/src/main/java/com/example/justin/password_validator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.justin.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void testPassword(View view){
+        EditText edit = (EditText)findViewById(R.id.editText);
+        TextView text = (TextView)findViewById(R.id.textView);
+        String password = edit.getText().toString();
+        if(passwordStrength(password))
+            text.setText("Password is Strong");
+        else
+            text.setText("Password is Weak");
     }
     public static boolean passwordStrength(String password){
         boolean containsLowerCase = false;
